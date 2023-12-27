a151c500a3744c43b25de9a2fa55caf8ee88afb0
Nadège Moisan <nadege@MacBook-Pro-de-Nadege.local>
2023-12-27 17:26:18 -0500

    ajout

--- inc/so_long.h
+++ inc/so_long.h
@@ -3,17 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namoisan <namoisan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nadege <nadege@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 15:34:42 by namoisan          #+#    #+#             */
-/*   Updated: 2023/12/20 11:45:31 by namoisan         ###   ########.fr       */
+/*   Updated: 2023/12/27 13:51:49 by nadege           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "./My_Library/libft.h"
+# include "./libft/libft.h"
+# include "MLX42/include/MLX42/MLX42.h"
+# include "MLX42/include/MLX42/MLX42_Int.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
@@ -21,5 +23,19 @@
 # define TRUE 1
 # define FALSE 0
 # define ERROR -1
+
+typedef struct s_game
+{
+	char	**map;
+	int		width;
+	int		height;
+	int		moves;
+	int		collectable;
+}	t_game;
+
+typedef struct s_data
+{
+	t_game	*game;
+}	t_data;
 
 #endif
